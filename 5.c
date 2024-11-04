8518dcc26b4ae757da09e07fa6a1504404330e93
Alba Costas <albacostasfernandez@gmail.com>
2024-11-04 16:10:46 +0100

    Codigo comentadooooo

--- 5.c
+++ 5.c
@@ -1,3 +1,18 @@
+/*  
+    Si el numero de hilos empleado en el programa es superior al número de cores de mi equipo,
+    en esta caso 16 cores; el tiempo de ejecución aumenta en comparación con una ejecución en la
+    que empleo extactamente 16 cores. Esto sucede ya que el sistema operativo distribuye el tiempo
+    de CPU entre todos los hilos, lo que significa que los hilos se bloqueen y mientras que este
+    está bloqueado deja que otros hilos, que estaban esperando a que se libere algun núcleo, 
+    utilicen la CPU. Este proceso de intercambio entre hilos genera un sobrecarga que termina 
+    ralentizando la ejecución del programa.
+    
+    El uso de un gran numero de hilos no contribuye a una mayor velocidad, sino que hace que cada
+    hilo pase más tiempo esperando en cola a que un núcleo se libere; por lo tanto, tener más hilos
+    que núcleos en este programa no mejora el rendimiento, de hecho, lo empeora. Aun así, sigue
+    habiendo ejecución en paralelo.
+*/
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,7 +46,6 @@ void *funcion(void *args) {
             sumaParcial += 1.0/(i*i);
         }
 
-       // printf("Hilo %d, suma parcial de %d a %d = %f\n", idHilo, inicio, fin, sumaParcial);
         sumaTotalHilo += sumaParcial;
     }
 
@@ -71,15 +85,17 @@ int main() {
     printf("\n\nSuma paralela: %.18f\n", sumaParalela);
 
 
-    //sumaSecuencial = 0;
+    // Calculamos la suma total secuencial en el hilo principal.
     for (double i = 1.0; i <= TOTAL_ITERACIONES; i+=1.0) {
         sumaSecuencial += 1.0/(i*i);
     }   
     printf("\nSuma secuencial: %.18f\n", sumaSecuencial);
 
+    // Calculamos la diferencia entre la suma secuencial y suma paralela.
     diferenciaSumas = sumaSecuencial - sumaParalela;
     printf("La diferencia entre la suma secuencia y paralela es: %.18f\n", diferenciaSumas);
 
+    // Calculamos la diferencia entre la suma paralela y el valor exacto 
     diferenciaValorExacto =  sumaParalela - (M_PI*M_PI)/6;
     printf("La diferencia entre la suma paralela y el valor exacto es: %.18f\n", diferenciaValorExacto);
 
