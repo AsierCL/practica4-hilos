b3e2f7e8673edd891c8a051290539f2ed449d88a
AsierCL <asiercl.05@gmail.com>
2024-11-04 12:16:06 +0100

    Antes de pull

--- 5.c
+++ 5.c
@@ -5,9 +5,12 @@
 #include <sched.h>
 
 #define nHilos 16
+float datos[nHilos];
 
 void *funcion(void* args){
-    
+    int idHilo = *(int *)args;
+    printf("Id de hilo:%d\n",idHilo);
+    datos[idHilo]=idHilo*0.1f;
 }
 
 
@@ -21,6 +24,8 @@ int main(int argc, char const *argv[]){
     for(int i=0; i<nHilos-1; i++){
         pthread_join(hilos[i], NULL);
     }
+
+    printf("Total: %f",datos[3]);
 
     return 0;
 }
